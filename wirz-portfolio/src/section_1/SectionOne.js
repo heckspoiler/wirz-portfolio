4e84d76a4baf76f728d6e79f69d6991952ec4ec1
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-08 23:35:15 +0200

    added animation, section 1 finished

--- wirz-portfolio/src/section_1/SectionOne.js
+++ wirz-portfolio/src/section_1/SectionOne.js
@@ -3,36 +3,70 @@ import "./SectionOne.css";
 import heroImage from "./img/hero-image.png";
 import star from "./icons/large-star.png";
 import arrow from "./icons/arrow-first-section.png";
+import { motion as m } from "framer-motion";
 
 export const SectionOne = () => {
   return (
-    <div className="section-one_main-container">
-      <div className="section-one_left-side-container">
-        <img src={star} className="section-one_star" />
-        <h1 className="section-one_main-title">
+    <m.div
+      className="section-one_main-container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      <section className="section-one_left-side-container">
+        <m.img
+          src={star}
+          className="section-one_star"
+          initial={{ height: 0, width: 0 }}
+          animate={{ height: "3rem", width: "3rem" }}
+          transition={{ duration: 1 }}
+        />
+        <m.h1
+          className="section-one_main-title"
+          initial={{ fontSize: "3rem", marginLeft: "-10rem" }}
+          animate={{ fontSize: "4rem", marginLeft: "0" }}
+          transition={{ duration: 1 }}
+        >
           HEY <br /> WIRZ!
-        </h1>
-        <div className="section-one_paragraph-container">
+        </m.h1>
+        <section className="section-one_paragraph-container">
           <p className="section-one-paragraph">
             I understand that you are currently in need of a
           </p>
-          <div className="section-one-paragraph-emphasize-container">
+          <section className="section-one-paragraph-emphasize-container">
             <p className="section-one-paragraph-emphasize">
               Junior Web Developer
             </p>
-          </div>
+          </section>
           <p className="section-one-paragraph">
             and I would like to work as such!
           </p>
-        </div>
-      </div>
-      <div className="section-one_lower-paragraph-container">
+        </section>
+      </section>
+      <m.section
+        className="section-one_lower-paragraph-container"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1.5 }}
+        transition={{ duration: 1, delay: 2 }}
+      >
         <p className="section-one_lower-paragraph">
           Maybe we'll find eachother
         </p>
-      </div>
-      <img src={heroImage} className="section-one_image" />
-      <img src={arrow} className="section-one_arrow" />
-    </div>
+      </m.section>
+      <m.img
+        src={heroImage}
+        className="section-one_image"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8, delay: 1 }}
+      />
+      <m.img
+        src={arrow}
+        className="section-one_arrow"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1.5 }}
+        transition={{ duration: 1, delay: 3 }}
+      />{" "}
+    </m.div>
   );
 };
