e8ae172e8ec4195fbf42ccc56baa726b219b2bb9
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-08 22:26:40 +0200

    adding arrow

--- wirz-portfolio/src/section_1/SectionOne.js
+++ wirz-portfolio/src/section_1/SectionOne.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SectionOne.css";
 import heroImage from "./img/hero-image.png";
+import star from "./icons/large-star.png";
 
 export const SectionOne = () => {
   return (
     <div className="section-one_main-container">
       <div className="section-one_left-side-container">
+        <img src={star} className="section-one_star" />
         <h1 className="section-one_main-title">
           HEY <br /> WIRZ!
         </h1>
@@ -13,7 +15,7 @@ export const SectionOne = () => {
           <p className="section-one-paragraph">
             I understand that you are currently in need of a
           </p>
-          <div>
+          <div className="section-one-paragraph-emphasize-container">
             <p className="section-one-paragraph-emphasize">
               Junior Web Developer
             </p>
@@ -22,6 +24,11 @@ export const SectionOne = () => {
             and I would like to work as such!
           </p>
         </div>
+      </div>
+      <div className="section-one_lower-paragraph-container">
+        <p className="section-one_lower-paragraph">
+          Maybe we'll find eachother
+        </p>
       </div>
       <img src={heroImage} className="section-one_image" />
     </div>
