66a79e090ea7df5f0ec384fb29933a0b01bc3277
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-09 12:05:17 +0200

    section two finished

--- wirz-portfolio/src/App.js
+++ wirz-portfolio/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./App.css";
 import { SectionOne } from "./section_1/SectionOne";
+import { SectionTwo } from "./section_2/SectionTwo";
 
 function App() {
   return (
     <div className="App">
       <SectionOne />
+      <SectionTwo />
     </div>
   );
 }
