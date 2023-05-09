5eafc092ada712f1deb4dbf378cb2c288dba6eba
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-10 01:15:23 +0200

    almost all animations added

--- wirz-portfolio/src/section_5/SectionFive.js
+++ wirz-portfolio/src/section_5/SectionFive.js
@@ -9,12 +9,54 @@ import ellipseFive from "./img/ellipse_section-five.png";
 import roadmap from "./img/roadmap.png";
 
 export const SectionFive = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.3, // Adjust the value between 0 and 1 according to when you want the animation to trigger
+    triggerOnce: true, // Set to true so the animation only triggers once when the element comes into view
+  });
+
+  const animationTitleFifth = useAnimation();
+  const animationCircleOverlay = useAnimation();
+  const animationRoadmap = useAnimation();
+  const animationEllipseFive = useAnimation();
+  const animateParagraph = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animationTitleFifth.start({
+        opacity: 1,
+        transition: { type: "spring", duration: 1.5, bounce: 0.3, delay: 1 },
+      });
+      animationCircleOverlay.start({
+        height: "0px",
+        width: "0px",
+        transition: { type: "spring", duration: 1.5, bounce: 0.3, delay: 1 },
+      });
+      animationRoadmap.start({
+        left: "50%",
+        transition: { type: "spring", duration: 1.5, bounce: 0.3, delay: 1.4 },
+      });
+      animationEllipseFive.start({
+        opacity: 1,
+        transition: { type: "spring", duration: 1.5, bounce: 0.3, delay: 1.8 },
+      });
+    }
+  }, [inView]);
   return (
-    <div className="section-five_container">
-      <m.h1 className="section-five_title">Education</m.h1>
+    <div className="section-five_container" ref={ref}>
+      <m.h1 className="section-five_title" animate={animationTitleFifth}>
+        Education
+      </m.h1>
+      <m.div
+        className="section-five_circle-overlay"
+        animate={animationCircleOverlay}
+      ></m.div>
       <m.img src={circles} className="section-five_circles" />
-      <m.img src={ellipseFive} className="section-five_ellipse" />
-      <m.img src={roadmap} className="roadmap" />
+      <m.img
+        src={ellipseFive}
+        className="section-five_ellipse"
+        animate={animationEllipseFive}
+      />
+      <m.img src={roadmap} className="roadmap" animate={animationRoadmap} />
       <m.p className="section-five_paragraph">
         So many wrong turns to get on the right track, huh...
       </m.p>
