dcb9c556be0e1f973e73236dc90d2af9bad665e8
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-09 23:16:19 +0200

    section four more or less finished

--- wirz-portfolio/src/section_4/SectionFour.js
+++ wirz-portfolio/src/section_4/SectionFour.js
@@ -24,6 +24,7 @@ export const SectionFour = () => {
   const animationThird = useAnimation();
   const animationFourth = useAnimation();
   const animationText = useAnimation();
+  const animationImage = useAnimation();
 
   useEffect(() => {
     if (inView) {
@@ -51,6 +52,10 @@ export const SectionFour = () => {
         opacity: 1,
         transition: { type: "spring", duration: 3, bounce: 0.3, delay: 2.6 },
       });
+      animationImage.start({
+        opacity: 1,
+        transition: { type: "spring", duration: 3, bounce: 0.3, delay: 3.2 },
+      });
     }
   }, [inView]);
 
@@ -59,6 +64,11 @@ export const SectionFour = () => {
       <m.h1 className="section-four_title" animate={animationTitleFourth}>
         WORK EXPERIENCE
       </m.h1>
+      <m.img
+        src={ellipse}
+        className="section-four_ellipse"
+        animate={animationImage}
+      />
       <table className="section-four_job-table">
         <tr className="section-four_image-row">
           <td>
@@ -171,75 +181,6 @@ export const SectionFour = () => {
           </td>
         </tr>
       </table>
-      {/* <section className="section-four_job-container">
-        <section className="section-four_columns">
-          <section className="section-four_job first-job-container passugger">
-            <m.img
-              src={passugger}
-              className="img-passugger"
-              animate={animationFirst}
-            />
-            <m.p className="section-four_job-date" animate={animationFirst}>
-              2011 - 2012
-            </m.p>
-            <h2 className="section-four_job-subheader">
-              Tasks & Responsibilities
-            </h2>
-            <ul className="section-four_job-list">
-              <li>Backoffice / Administration</li>
-              <li>Production</li>
-              <li>Warehouse Work</li>
-              <li>Assistant</li>
-            </ul>
-          </section>
-          <section className="section-four_job second-job-container schwarz">
-            <m.img
-              src={schwarz}
-              className="img-schwarz"
-              animate={animationSecond}
-            />
-            <m.p className="section-four_job-date" animate={animationSecond}>
-              2012 - 2014
-            </m.p>
-            <h2 className="section-four_job-subheader">
-              Tasks & Responsibilities
-            </h2>
-            <ul>
-              <li>Backoffice / Administration</li>
-              <li>HR</li>
-            </ul>
-          </section>
-        </section>
-        <section className="section-four_columns">
-          <section className="section-four_job third-job-container parki">
-            <m.img src={parki} className="img-parki" animate={animationThird} />
-            <m.p className="section-four_job-date" animate={animationThird}>
-              2018-2021
-            </m.p>
-            <h2 className="section-four_job-subheader">
-              Tasks & Responsibilities
-            </h2>
-            <ul>
-              <li>Bartender</li>
-            </ul>
-          </section>
-          <section className="section-four_job fourth-job-container rio">
-            <m.img src={rio} className="img-rio" animate={animationFourth} />
-            <m.p className="section-four_job-date" animate={animationFourth}>
-              2013-2018 / 2021 -2023
-            </m.p>
-            <h2 className="section-four_job-subheader">
-              Tasks & Responsibilities
-            </h2>
-            <ul>
-              <li>Shift Manager / Deputy Manager</li>
-              <li>Orders</li>
-              <li>Bartender</li>
-              <li>Runner</li>
-            </ul>
-          </section>
-        </section>
-      </section> */}
     </div>
   );
 };
