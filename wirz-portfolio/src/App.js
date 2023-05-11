c3b17aa82880416223a944e8567c0f4dc1bb8686
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-05-11 14:09:43 +0200

    changes

--- wirz-portfolio/src/App.js
+++ wirz-portfolio/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React, { useRef, useEffect } from "react";
 import "./App.css";
+
 import { SectionOne } from "./section_1/SectionOne";
 import { SectionTwo } from "./section_2/SectionTwo";
 import { SectionThree } from "./section_3/SectionThree";
@@ -10,6 +10,18 @@ import { SectionSix } from "./section_6/SectionSix";
 import { SectionSeven } from "./section_7/SectionSeven";
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      console.log("Scroll position:", scrollPosition);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="App">
       <SectionOne />
